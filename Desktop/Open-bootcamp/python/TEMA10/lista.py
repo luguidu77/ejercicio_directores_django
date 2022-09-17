508f27b3bea80cf3d744fdde6b9e4f460822d424
Juan Lugo <luguidu77@gmail.com>
2022-09-17 18:41:04 +0200

    ejercicio 10 lista

---
+++ Desktop/Open-bootcamp/python/TEMA10/lista.py
@@ -0,0 +1,44 @@
+import tkinter
+from tkinter import ttk
+
+
+from tkinter import *
+
+window = tkinter.Tk()
+window.columnconfigure(0, weight=200)
+window.config(width=150, padx=50)
+
+frame = ttk.Frame(window)
+frame.grid(column=0, row=0)
+
+
+
+label = ttk.Label(frame, text='Lista seleccionable:')
+label.grid(column=0, row=0)
+
+
+def imprimeSeleccion(texto):
+   
+
+    print('selecionado: ' + texto)
+
+
+def limpiar():
+    print('limpiado')
+    
+   
+lista = ['España', 'Francia', 'Alemania', 'Italia']
+
+lista_item =tkinter.StringVar(value=lista)
+
+
+conbobox = tkinter.Listbox(frame, height=10, listvariable=lista_item)
+
+conbobox.grid(column=0, row=1, sticky=tkinter.W)
+
+
+
+
+
+
+window.mainloop()
